305f9fc8c1cb0fc5ee39c1ba6b8fdb537c6b47c3
adnrbp <adnrbp@users.noreply.github.com>
2018-12-15 19:03:16 -0500

    little modifications to selective - else sample

--- 01Basics/006Controls/selectiveSample.c
+++ 01Basics/006Controls/selectiveSample.c
@@ -1,14 +1,23 @@
 #include <stdio.h>
 
 void main(){
-	int a=10;
+	//int a=10;
 	int b=0;
 
+	/*
 	if(a > b){
 		printf("yes, A is bigger than B\n");
 		printf("Bye.\n");
 	}
 
 	printf("the app will close.\n");
+	*/
+	int a = 0;
+	//int a = 999;
+	if (a == 0)
+		printf("A is a number bigger than or equal to 0\n")
+	else
+		printf("A might be  any other number different than 0\n");
+
 
 }
